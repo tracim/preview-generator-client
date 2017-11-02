39e90dd903258645c64753c08dc3caeec95de6cc
Skylsmoi <come.huguies@gmail.com>
2017-11-02 17:32:28 +0100

    added handling for different language codes

--- trad.js
+++ trad.js
@@ -1,7 +1,19 @@
 let tradList = ''
 
 export function setLang (newLang) {
-  tradList = require('./translate/' + newLang + '.json')
+  const normalizedLang = (() => {
+    switch (newLang) {
+      case 'fr':
+      case 'fr_FR':
+      case 'fr-fr':
+        return 'fr_FR'
+      case 'en':
+      case 'en_EN':
+      case 'en_US':
+        return 'en_EN'
+    }
+  })()
+  tradList = require('./translate/' + normalizedLang + '.json')
 }
 
 export function __ (tradId) {
